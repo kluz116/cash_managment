ca8e36ed4232e3ac1d6efa4bf9ecf9d66a02a5dd
Allan Williams Musembya Kluz <engallanmusembya@gmail.com>
2022-03-04 17:06:43 +0300

    updates

--- models/cash_users.py
+++ models/cash_users.py
@@ -7,6 +7,6 @@ class res_users(models.Model):
     
     role = fields.Selection([('manager', 'Branch Manager'), ('accountant', 'Branch Accountant'),('cash_center', 'Cash Support Team'),('it_support', 'IT Support Team')], string="Role",required=True)
     manager = fields.Many2one('res.partner',string ='Manager')
-    branch_id = fields.Many2one('cash_managment.branch',string ='Branch', required=True)
+    branch_id = fields.Many2one('cash_managment.branch',string ='Branch')
 
 
